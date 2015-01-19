cd0f5790df8db80d48aa2533dc5f0f8a4ade5a6c
Rabea Gleissner <rabeagleissner@gmail.com>
2015-01-19 14:27:04 +0000

    added resources conversations to routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
-
+  resources :conversations
   resources :users do
     resources :dogs do
 
