983954cd3dea9c7dbf84349f62c712bb06096ab3
bvinel <b.vinel@gmail.com>
2015-01-19 12:23:18 +0000

    commits app with authorization error msg corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: "You can't access this page"
+    redirect_to :back, alert: "You can't access this page" 
   end
 
 end
