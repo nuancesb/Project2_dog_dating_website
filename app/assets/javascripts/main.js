6f7bcf94de516f949c87513bf030e25152191a5c
bvinel <b.vinel@gmail.com>
2015-01-19 14:44:51 +0000

    commits app with google map insert

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,23 @@
 
 var myApp = myApp || {};
 
+myApp.initMap = function() {
+ 
+  var mapOptions = {
+      center: { lat:  51.52, lng: -0.115},     
+      zoom: 14,
+      mapTypeId:google.maps.MapTypeId.ROADMAP //default
+  };
+
+    var map = new google.maps.Map(myApp.mapCanvas, mapOptions);
+  
+};
+
 myApp.setup = function() {
   console.log("Main JS loaded");
 };
 
 $(function() {
-  myApp.setup();
-
+  myApp.mapCanvas = $('#map-canvas')[0];
+  myApp.initMap();
 });
