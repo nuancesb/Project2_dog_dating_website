280b7c5b16c4eca93a785cf31f005abd16ecd83e
sophie <sophie.allaoua@hotmail.fr>
2015-01-20 10:13:17 +0000

    adding json handler to users_controller index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,17 @@ class UsersController < ApplicationController
 
   def index
     @user = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
   end
 
  
   def show
       @user = User.find(params[:id])
       @users = User.all
-      
+
     end
 
   def edit
