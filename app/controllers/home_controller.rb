a0a3b376e0d1502be542631f4690316fd5e01976
Rabea Gleissner <rabeagleissner@gmail.com>
2015-01-22 15:38:33 +0000

    added FAQ path in routes, corresponding controller action and template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-
-
       @q = Dog.ransack(params[:q])
       @dogs = @q.result(distinct: true)
+  end
 
+  def faq
+
+    @q = Dog.ransack(params[:q])
+    @dogs = @q.result(distinct: true)
   end
 
 
