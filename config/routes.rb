548ec52118341106c2392a130090834df2ec5f18
Rabea Gleissner <rabeagleissner@gmail.com>
2015-01-22 15:49:23 +0000

    changed messages display on user index to display a max of 5

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   root to: "home#index"
   #get '/users/:user_id/conversations/:id/reply', to: 'conversations#reply', as: 'reply_to_conversation'
-  get  '/home/faq', to: "home#faq", as: 'faq'
-  get  '/home/about', to: "home#about", as: 'about'
+
 
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  get  '/home/faq', to: "home#faq", as: 'faq'
+  get  '/home/about', to: "home#about", as: 'about'
   resources :conversations, only: [:index, :show, :destroy] do
        member do
         post 'reply'
